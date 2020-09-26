bad90c39b978b19fe553691630741b01ca910162
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-09-25 20:45:53 -0700

    github

---
+++ build/precache-manifest.6efec2b75c6d4be4511fa53a15d0149c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005196e179d706bfa2412d6b3923bb43",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "98cd74a883f47c1555f6",
+    "url": "/reactportfolio/static/css/main.81a938b1.chunk.css"
+  },
+  {
+    "revision": "0efaf0c1ddb6ac203cdc",
+    "url": "/reactportfolio/static/js/2.8cc031f9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactportfolio/static/js/2.8cc031f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98cd74a883f47c1555f6",
+    "url": "/reactportfolio/static/js/main.9904cfcc.chunk.js"
+  },
+  {
+    "revision": "ccc63777df2cb755b429",
+    "url": "/reactportfolio/static/js/runtime-main.ff3c2836.js"
+  }
+]);
