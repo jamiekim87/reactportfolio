d17956bfa505b4de78c2f55002aca3b0c06a18cf
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-09-28 15:52:35 -0700

    push to repo

---
+++ build/precache-manifest.3be2a85efe48ed7705cf26305aa62d66.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ee0f5209615269c6d4985a8054fa37f",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "e4ff4785d0a98a6c8e82",
+    "url": "/reactportfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "306628fd13a70af4c501",
+    "url": "/reactportfolio/static/css/main.ed13a0b2.chunk.css"
+  },
+  {
+    "revision": "e4ff4785d0a98a6c8e82",
+    "url": "/reactportfolio/static/js/2.07097c6f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactportfolio/static/js/2.07097c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306628fd13a70af4c501",
+    "url": "/reactportfolio/static/js/main.b7af55a6.chunk.js"
+  },
+  {
+    "revision": "ccc63777df2cb755b429",
+    "url": "/reactportfolio/static/js/runtime-main.ff3c2836.js"
+  },
+  {
+    "revision": "eef19e5aa5417f0b95f5d48c134a01ac",
+    "url": "/reactportfolio/static/media/bg.eef19e5a.png"
+  },
+  {
+    "revision": "a1003db7bfcc7fc5eaff1b99a0d9da02",
+    "url": "/reactportfolio/static/media/budget_tracker.a1003db7.png"
+  },
+  {
+    "revision": "e0ef7f5ff137b8a9758a4d67d2e1b148",
+    "url": "/reactportfolio/static/media/employee.e0ef7f5f.png"
+  },
+  {
+    "revision": "a5887b2b7b8f20a9a4ced3c8b905d3b7",
+    "url": "/reactportfolio/static/media/fitnesstracker.a5887b2b.png"
+  },
+  {
+    "revision": "3a4388735f681c7a759b08dde97f4a5e",
+    "url": "/reactportfolio/static/media/image1.3a438873.jpg"
+  },
+  {
+    "revision": "070f49c34b8682fd2b66d024c0a035a5",
+    "url": "/reactportfolio/static/media/img1.070f49c3.png"
+  },
+  {
+    "revision": "6b3eb72a157d5f81c323b43bd12a6aef",
+    "url": "/reactportfolio/static/media/lifetrack.6b3eb72a.jpg"
+  },
+  {
+    "revision": "e91dd5cf79815109fff67ccae533232b",
+    "url": "/reactportfolio/static/media/pokemon3.e91dd5cf.jpg"
+  }
+]);
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -10,30 +10,7 @@ const About = (props) => {
     <>
       <div>
       <div className="p-3 bg-info my-2 rounded">
-        <Toast>
-          <ToastHeader>
-            Reactstrap
-          </ToastHeader>
-          <ToastBody>
-            This is a toast on an info background — check it out!
-          </ToastBody>
-        </Toast>
-      </div>
-      <div className="p-3 my-2 rounded">
-        <Toast>
-          <img src={image} width="50%" width="260" height="265" />
-          <ToastHeader>
-            Jamie Kim
-          </ToastHeader>
-          <ToastBody>
-            I am a Full Stack Web Developer graduating from UCI Coding Bootcamp of 2020. Entering college about eight years ago, my parents told me that this is the time of your life where you decide
-            who you will become. Quite a scary thought for someone that liked little bits and pieces of each subject I
-            learned in school. I decided to major in Chemistry, something I did moderately well in high school and could
-            lead to the path of becoming a pharmacist, the phrase a lot of my peers around me mentioned more so because of
-            their parents then because of themselves. Nevertheless, my experience in college life led me to find genuine
-            interest in Computer as well as Web Development. I have posted my projects and several courseworks on my portfolio
-            page. Please contact me for further questions. Thank you!</ToastBody>
-        </Toast>
+      
       </div>
         <Jumbotron>
           <img src={Background} />
